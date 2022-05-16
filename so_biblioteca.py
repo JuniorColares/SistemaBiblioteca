d941a6eb50c99b3fd559134e47d9f654d573d6eb
Junior Colares <98316365+JuniorColares@users.noreply.github.com>
2022-05-16 20:48:17 -0300

    Add files via upload

--- so_biblioteca.py
+++ so_biblioteca.py
@@ -2,66 +2,88 @@ import conexaobiblioteca as connect
 import os
 
 #COLOCAR O NOME DO BANCO DE DADOS E A SENHA NOS LOCAIS INDICADOS
-conexao = connect.criar_conexao('localhost', 'NOME DO DATABASE','root','SENHA DE ACESSO')
+conexao = connect.criar_conexao('localhost', 'db_biblioteca','root','admin')
 
 if conexao.is_connected():
     cursor = conexao.cursor()
 
 while True:
     os.system('cls' if os.name == 'nt' else 'clear')
-    print("-----------------------------------------------------")
-    print("|              Escolha a opção desejada             |")
-    print("|                                                   |")
-    print("|                                                   |")
-    print("|                                                   |")
-    print("| (1) Cadastrar Livro                               |")
-    print("| (2) Consultar livros cadastrados                  |")
-    print("| (3) Consultar livro pelo título                   |")
-    print("| (4) Alugar livro                                  |")
-    print("| (5) Devolver livro                                |")
-    print("| (6) Alterar cadastro de livro                     |")
-    print("| (7) Excluir livro                                 |")
-    print("| (8) Encerrar sistema                              |")
-    print("|                                                   |")
-    print("|                                                   |")
-    print("|                                                   |")
-    print("-----------------------------------------------------")    
-    op = input('Oprção: ')
-
+    connect.menu_inicial()
+    op = input('Opção: ')
     if op == '1':
-        os.system('cls' if os.name == 'nt' else 'clear')
-        connect.cadastrar_livro(conexao, cursor)
-        a = input('')
-    elif op == '2':
-        os.system('cls' if os.name == 'nt' else 'clear')
-        connect.listar_livros(cursor)
-        a = input('')
-    elif op == '3':
-        os.system('cls' if os.name == 'nt' else 'clear')
-        connect.pesquisar_livro(cursor)
-        a = input('')
-    elif op == '4':
-        os.system('cls' if os.name == 'nt' else 'clear')
-        connect.alugar_livro(conexao, cursor)
-        a = input('')
-    elif op == '5':
-        os.system('cls' if os.name == 'nt' else 'clear')
-        connect.devolver_livro(conexao, cursor)
-        a = input('')
-    elif op == '6':
-        os.system('cls' if os.name == 'nt' else 'clear')
-        connect.alterar_livro(conexao, cursor)
-        a = input('')
-    elif op == '7':
-        os.system('cls' if os.name == 'nt' else 'clear')
-        connect.excluir_livro(conexao, cursor)
-        a = input('')
-    elif op == '8':
+        while True:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            connect.menu_livros()   
+            op = input('Oprção: ')
+
+            if op == '1':
+                os.system('cls' if os.name == 'nt' else 'clear')
+                connect.cadastrar_livro(conexao, cursor)
+                a = input('')
+            elif op == '2':
+                os.system('cls' if os.name == 'nt' else 'clear')
+                connect.listar_livros(cursor)
+                a = input('')
+            elif op == '3':
+                os.system('cls' if os.name == 'nt' else 'clear')
+                connect.pesquisar_livro(cursor)
+                a = input('')
+            elif op == '4':
+                os.system('cls' if os.name == 'nt' else 'clear')
+                connect.alugar_livro(conexao, cursor)
+                a = input('')
+            elif op == '5':
+                os.system('cls' if os.name == 'nt' else 'clear')
+                connect.devolver_livro(conexao, cursor)
+                a = input('')
+            elif op == '6':
+                os.system('cls' if os.name == 'nt' else 'clear')
+                connect.alterar_livro(conexao, cursor)
+                a = input('')
+            elif op == '7':
+                os.system('cls' if os.name == 'nt' else 'clear')
+                connect.excluir_livro(conexao, cursor)
+                a = input('')
+            elif op == '8':
+                break
+            else:
+                print('Opção Inexistente!')
+                a = input('')
+    if op == '2':
+        while True:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            connect.menu_associados() 
+            op = input('Opção: ')
+            if op == '1':
+                os.system('cls' if os.name == 'nt' else 'clear')
+                connect.cadatrar_associado(conexao, cursor)
+                print('Cadastro realizado!')
+                a = input('')            
+            if op == '2':
+                os.system('cls' if os.name == 'nt' else 'clear')
+                connect.buscar_associado(cursor)
+                a = input('')
+            if op == '3':
+                os.system('cls' if os.name == 'nt' else 'clear')
+                connect.alterar_associado(conexao,cursor)
+                a = input('')
+            if op == '4':
+                os.system('cls' if os.name == 'nt' else 'clear')
+                connect.excluir_associado(conexao, cursor)
+                a = input('')
+            if op == '5':
+                break
+
+    if op == '3':
+        pass
+    
+    if op == '4':
         break
     else:
-        print('Opção Inexistente!')
+        print('Comando inválido!')
         a = input('')
-
+    
 if conexao.is_connected():
     conexao.close()
     os.system('cls' if os.name == 'nt' else 'clear')
